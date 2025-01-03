3334a630fff41c9f922e3b1c44ecf7a217ba2e76
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2025-01-03 16:34:48 +0530

    Changes to be committed: 	new file:   chatapp/lib/components/image_message_bubble.dart 	modified:   chatapp/lib/components/message_bubbles.dart 	new file:   chatapp/lib/components/video_message_bubble.dart 	new file:   chatapp/lib/components/vm_message_bubbles.dart 	modified:   chatapp/lib/pages/navBar/chat/chat.dart

--- chatapp/lib/pages/navBar/chat/chat.dart
+++ chatapp/lib/pages/navBar/chat/chat.dart
@@ -1,4 +1,7 @@
+import 'package:chatapp/components/image_message_bubble.dart';
 import 'package:chatapp/components/message_bubbles.dart';
+import 'package:chatapp/components/video_message_bubble.dart';
+import 'package:chatapp/components/vm_message_bubbles.dart';
 import 'package:chatapp/pages/navBar/chat/hooks/chat_hooks.dart';
 import 'package:chatapp/providers/user_provider.dart';
 import 'package:chatapp/utils/env.dart';
